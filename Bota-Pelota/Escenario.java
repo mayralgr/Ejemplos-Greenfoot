ed32221670bb40a1d7709c90cdc92eecb955f408
LuceroG <ml17ft@gmail.com>
2017-03-15 11:04:26 -0600

    Bota Pelota
    Agregar pelota al dar click en el mundo

---
+++ Bota-Pelota/Escenario.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escenario extends World
+{
+    public Escenario()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1,true); 
+    }
+    
+    @Override
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this))
+        {
+            System.out.println("Click en el mundo");
+            MouseInfo raton = Greenfoot.getMouseInfo();
+            System.out.println("Coordenadas x= "+
+            raton.getX()+" Coordenadas y= "+
+            raton.getY());
+        }
+    }
+}
---
+++ Bota-Pelota/Pelota.java
@@ -0,0 +1,55 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pelota here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pelota extends Actor
+{
+    private int incx;
+    private int incy;
+    
+    public Pelota()
+    {
+        incx=10;
+        incy=10;
+    }
+    /**
+     * Act - do whatever the Pelota wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    @Override
+    public void act() 
+    {
+        int posx=this.getX();
+        int posy = this.getY();// Add your action code here.
+        int nuevox=posx+incx;
+        int nuevoy=posy+incy;
+        //cambiamos de posicion a la pelota
+        
+        World mundo=this.getWorld();
+        if(nuevox>mundo.getWidth()) //rebota en el lado derecho
+        {
+            incx=-incx;
+        }
+        if(nuevoy>mundo.getHeight())//rebota en la parte de abajo
+        {
+            incy=-incy;
+        }
+        if(nuevoy<0)
+        {
+            incy=-incy;
+        }
+         if(nuevox<0)
+        {
+            incx=-incx;
+        }
+        
+        
+        
+        this.setLocation(nuevox,nuevoy);
+    }    
+    
+}
